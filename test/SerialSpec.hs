147b80742d28db345b692b813a922a997fd46190
Sebastian Sangervasi <s.ebastian.sangervasi@gmail.com>
2018-12-19 18:04:12 -0800

    Playing with DRY tests

--- test/SerialSpec.hs
+++ test/SerialSpec.hs
@@ -7,71 +7,77 @@ import Conway.GameOfLife
 import Conway.Seeds
 import Conway.Serial
 
-puts = putStrLn
-
 spec :: Spec
 spec = do
   describe "golToString" $ do
-    context "with an empty game" $ do
-      let gol = emptyGoL
+    let testTitle gol expected =
+          unlines [
+              "Board:"
+            , (show gol)
+            , "Should become:"
+            , expected
+          ]
 
-      it "is an empty string" $ do
-        golToString gol `shouldBe` ""
+        toStringTest gol expected =  
+          it (testTitle gol expected) $ do
+            golToString gol `shouldBe` expected
+
+    context "with an empty game" $ do
+      toStringTest emptyGoL ""
 
     context "with a glider" $ do
-      let gol = glider
-
-      it "matches the glider string" $ do
-        puts "Output:"
-        puts $ golToString gol
-        golToString gol `shouldBe` gliderString
+      toStringTest glider gliderString
 
   describe "stringToGoL" $ do
-    context "with an empty string" $ do
-      let golString = ""
+    let testTitle golString expected = 
+          unlines [
+              "String board:"
+            , golString
+            , "Should become:"
+            , (show expected)
+          ]
 
-      it "is an empty board" $ do
-        stringToGoL golString `shouldBe` emptyGoL
+        toGolTest golString expected =
+          it (testTitle golString expected) $ do
+            stringToGoL golString `shouldBe` expected
+
+    context "with an empty string" $ do
+      toGolTest "" emptyGoL
 
     context "with a glider" $ do
-      let golString = gliderString
-
-      it "matches the glider board" $ do
-        puts "Output:"
-        puts $ golString
-        print glider
-        print $ stringToGoL golString
-        stringToGoL golString `shouldBe` glider
+      toGolTest gliderString glider
 
   describe "chunksOf" $ do
     let testTitle size list expected = 
-          (    (show size)
-            ++ " | "
-            ++ (show list)
-            ++ " -> "
-            ++ (show expected)
-          )
+          concat [
+              (show size)
+            , " | "
+            , (show list)
+            , " -> "
+            , (show expected)
+          ]
+
+        chunkTest size list expected = 
+          it (testTitle size list expected) $ do
+            chunksOf size list `shouldBe` expected
 
     context "size 1 of []" $ do
       let size = 1
           list = [] :: [Int]
           expected = [] :: [[Int]]
         
-      it (testTitle size list expected) $ do
-        chunksOf size list `shouldBe` expected
+      chunkTest size list expected
 
     context "size 1 of [1..3]" $ do
       let size = 1
           list = [1, 2, 3]
           expected = [[1], [2], [3]]
         
-      it (testTitle size list expected) $ do
-        chunksOf size list `shouldBe` expected
+      chunkTest size list expected
 
     context "size 2 of [1..5]" $ do
       let size = 2
           list = [1, 2, 3, 4, 5]
           expected = [[1, 2], [3, 4], [5]]
         
-      it (testTitle size list expected) $ do
-        chunksOf size list `shouldBe` expected
+      chunkTest size list expected
